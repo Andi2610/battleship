704bfec1d3c820db56e966fb4dcd4e7bd5ee421e
Priyal Gupta <priyal.gupta@crestdatasys.com>
2018-01-18 13:58:30 +0530

    Ship Positions

--- server.py
+++ server.py
@@ -13,56 +13,84 @@ class Server:
 			thread.start()
 		except Exception as e:
 			print (e.message)
-	
-	def waitingConnect(self,serversocket):
-		n=0
-		while True:
-		conn,addr=serversocket.accept()
-			if n==1:
-				self.player1,addr1=conn,addr
-				thread1=threading.Thread(target=self.startGame(self.player1))
-				thread1.start()
-				self.player1.send("Welcome in Battleship game...Wait for player2...")
-			n+=1
-			if (n==2):
-				self.player2,addr2=conn,addr
-				thread2=threading.Thread(target=self.startGame(self.player2))
-				thread2.start()
-				self.player1.send("Be ready to fight")
-				self.player2.send("Be ready to fight")
-				n=0
-
+			
 		
 	
-	#enter ship positions
-	def enterShipPositions(self,serversocket):
-		self.player1.send("Enter ship positions")
-		self.player2.send("Enter ship positions")
-		self.player1_ship=dict()
-		self.player2_ship=dict()
+	#store ship positions
+	def store_ship_positions(self):
+		print (type(self.player1))
 		try:
-			for i in range(5):
-				self.player1_ship[(i+1)]=str(serversocket.recv(BUFFER))
+			player1_dict=self.player1.recv(BUFFER)
 		except:
 			print ("Player1 is unreachable")
 			return
 		try:
-			for i in range(5):
-				self.player2_ship[(i+1)]=str(serversocket.recv(BUFFER))
+			player2_dict=self.player2.recv(BUFFER)
 		except:
 			print ("Player2 is unreachable")
 			return
+
+		
+		self.player1_matrix=[[0,0,0,0,0,0,0,0,0,0]]*10
+		self.player2_matrix=[[0,0,0,0,0,0,0,0,0,0]]*10
+		
+		for key,value in player1_dict.items():
+			x=int(value[0])
+			y=int(value[1])
+			direction=str(value[2])
+			if direction=='h':
+				for i in range(x,x+key):
+					self.player1_matrix[i][y]=1
+					
+			if direction=='v':
+				for i in range(y,y+key):
+					self.player1_matrix[x][i]=1
+		
+		for key,value in player2_dict.items():
+			x=int(value[0])
+			y=int(value[1])
+			direction=str(value[2])
+			if direction=='h':
+				for i in range(x,x+key):
+					self.player2_matrix[i][y]=1
+					
+			if direction=='v':
+				for i in range(y,y+key):
+					self.player2_matrix[x][i]=1
+			
+		print (self.player1_matrix)
+		print (self.player2_matrix)
 	
-	def attackingposition(self,serversocket):
+	def attacking_position(self,serversocket):
 		self.player1.send("Enter attacking positions: ")
-		self.player2.send("Enter attacking positions: ")
+		#self.player2.send("Enter attacking positions: ")
 		
 		self.player1_attack=str(serversocket.recv(BUFFER))
-		self.player1_attack=str(serversocket.recv(BUFFER))
+		#self.player1_attack=str(serversocket.recv(BUFFER))
+	
+	def waitingConnect(self,serversocket):
+		n=0
+		while True:
+			conn,addr=serversocket.accept()
+			n+=1
+			if n==1:
+				self.player1,addr1=conn,addr
+				#self.player1.send("Welcome in Battleship game...Wait for player2...")
+			if (n==2):
+				self.player2,addr2=conn,addr
+				self.player1.send("Be ready to fight")
+				self.player2.send("Be ready to fight")
+				thread1=threading.Thread(target=self.startGame(self.player1))
+				thread1.start()
+				thread2=threading.Thread(target=self.startGame(self.player2))
+				thread2.start()
+				n=0
+
+	
 		
 	def startGame(self,serversocket):
-		enterShipPositions(self,serversocket)
-		attackingposition(self,serversocket)
+		self.store_ship_positions(serversocket)
+		self.attacking_position(serversocket)
 		
 		
 	
@@ -82,6 +110,7 @@ if  __name__=='__main__':
 
 	except socket.error as err:
 		print ("Socket creation failed with an error %s"%(err))
-	server=Server(serversocket)
+	while True:
+		server=Server(serversocket)
 		
 	
